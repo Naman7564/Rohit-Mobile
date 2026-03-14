ea082ec551b32dbaa2d15d6a931fe11a85653aa3
Naman7564 <Naman7564@gmail.com>
2026-03-14 11:57:56 +0530

    Fix static files serving

--- rohit_mobile_project/settings.py
+++ rohit_mobile_project/settings.py
@@ -95,8 +95,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'rohit_mobile_project', 'static')
+]
 
 # Media files
 MEDIA_URL = '/media/'
